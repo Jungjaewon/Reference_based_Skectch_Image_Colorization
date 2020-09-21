0d22e09916302d9fad9d40937d5ff611ee890adb
woodcook486 <woodcook486@naver.com>
2020-09-22 00:04:26 +0900

    implements SCFT and feature concatenation

--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 import torch.nn as nn
 import torch
-
+import math
 from block import ConvDownBlock
 
 
@@ -59,12 +59,11 @@ class Encoder(nn.Module):
         self.layer7 = ConvDownBlock(64, 128, spec_norm, stride=2, LR=LR)
         self.layer8 = ConvDownBlock(128, 128, spec_norm, LR=LR)
         self.layer9 = ConvDownBlock(128, 256, spec_norm, stride=2, LR=LR)
-        self.layer10 = ConvDownBlock(128, 256, spec_norm, LR=LR)
-
+        self.layer10 = ConvDownBlock(256, 256, spec_norm, LR=LR)
+        self.down_sampling = nn.AdaptiveAvgPool2d((16, 16))
 
     def forward(self, x):
 
-        feature_map_list = list()
         feature_map1 = self.layer1(x)
         feature_map2 = self.layer2(feature_map1)
         feature_map3 = self.layer3(feature_map2)
@@ -76,16 +75,119 @@ class Encoder(nn.Module):
         feature_map9 = self.layer9(feature_map8)
         feature_map10 = self.layer10(feature_map9)
 
-        feature_map_list.append(feature_map1)
-        feature_map_list.append(feature_map2)
-        feature_map_list.append(feature_map3)
-        feature_map_list.append(feature_map4)
-        feature_map_list.append(feature_map5)
-        feature_map_list.append(feature_map6)
-        feature_map_list.append(feature_map7)
-        feature_map_list.append(feature_map8)
-        feature_map_list.append(feature_map9)
-        feature_map_list.append(feature_map10)
+        down_feature_map1 = self.down_sampling(feature_map1)
+        down_feature_map2 = self.down_sampling(feature_map2)
+        down_feature_map3 = self.down_sampling(feature_map3)
+        down_feature_map4 = self.down_sampling(feature_map4)
+        down_feature_map5 = self.down_sampling(feature_map5)
+        down_feature_map6 = self.down_sampling(feature_map6)
+        down_feature_map7 = self.down_sampling(feature_map7)
+        down_feature_map8 = self.down_sampling(feature_map8)
 
-        return feature_map_list
+        """
+        print("feature_map1.size : ", self.down_sampling(feature_map1).size())  
+        print("feature_map2.size : ", self.down_sampling(feature_map2).size()) 
+        print("feature_map3.size : ", self.down_sampling(feature_map3).size())
+        print("feature_map4.size : ", self.down_sampling(feature_map4).size())
+        print("feature_map5.size : ", self.down_sampling(feature_map5).size())
+        print("feature_map6.size : ", self.down_sampling(feature_map6).size())
+        print("feature_map7.size : ", self.down_sampling(feature_map7).size())
+        print("feature_map8.size : ", self.down_sampling(feature_map8).size())
+        print("feature_map9.size : ", feature_map9.size())
+        print("feature_map10.size : ", feature_map10.size())
+        """
 
+        output = torch.cat([down_feature_map1,
+                            down_feature_map2,
+                            down_feature_map3,
+                            down_feature_map4,
+                            down_feature_map5,
+                            down_feature_map6,
+                            down_feature_map7,
+                            down_feature_map8,
+                            feature_map9,
+                            feature_map10,
+                            ], dim=1)
+
+        feature_list = [down_feature_map1,
+         down_feature_map2,
+         down_feature_map3,
+         down_feature_map4,
+         down_feature_map5,
+         down_feature_map6,
+         down_feature_map7,
+         down_feature_map8,
+         feature_map9,
+         feature_map10,
+         ]
+        #print('output.size : ', output.size()) # output.size :  torch.Size([2, 992, 16, 16])
+        b, ch, h, w = output.size()
+        #output = output.reshape((b, ch, h * w)) # output.size :  torch.Size([2, 992, 256])
+        output = output.reshape((b, h * w, ch)) # output.size :  torch.Size([2, 256, 992])
+        #print('output.size : ', output.size())
+        return output, feature_list
+
+class Decoder(nn.Module):
+    """Discriminator network with PatchGAN.
+    W = (W - F + 2P) /S + 1"""
+
+    def __init__(self, in_channels=3, spec_norm=False, LR=0.2):
+        super(Decoder, self).__init__()
+
+    def forward(self, x):
+        return x
+
+
+class SCFT_Moudle(nn.Module):
+    """Discriminator network with PatchGAN.
+    W = (W - F + 2P) /S + 1"""
+
+    def __init__(self):
+        super(SCFT_Moudle, self).__init__()
+        self.w_q = nn.Linear(992, 992)
+        self.w_k = nn.Linear(992, 992)
+        self.w_v = nn.Linear(992, 992)
+        self.scailing_factor = math.sqrt(992)
+        self.softmax = nn.Softmax(dim=2)
+
+    def forward(self, v_s, v_r):
+
+        #print('v_s.size() : ', v_s.size()) # v_s.size() :  torch.Size([2, 256, 992])
+        #print('v_r.size() : ', v_r.size()) # v_r.size() :  torch.Size([2, 256, 992])
+        q_result = self.w_q(v_s)
+        k_result = self.w_k(v_r)
+        v_result = self.w_v(v_r)
+
+        #print('q_result : ', q_result.size()) # q_result :  torch.Size([2, 256, 992])
+        #print('k_result : ', k_result.size()) # k_result :  torch.Size([2, 256, 992])
+        k_result = k_result.permute(0, 2, 1)
+        #print('k_result : ', k_result.size()) # k_result :  torch.Size([2, 992, 256])
+        attention_map = torch.bmm(q_result, k_result)
+        #print('attention_map : ', attention_map.size()) # attention_map :  torch.Size([2, 256, 256])
+        attention_map = self.softmax(attention_map) / self.scailing_factor
+        v_star = torch.bmm(attention_map, v_result)
+        #print('v_star.size() : ', v_star.size()) # v_star.size() :  torch.Size([2, 256, 992])
+        """
+        *debug example
+        soft_max = nn.Softmax(dim=2)
+        t = torch.randn((2,5,5))
+        softed_t = soft_max(t)
+        print(softed_t)
+        print(torch.sum(softed_t[0, 0]))
+        """
+        v_sum = (v_s + v_star).permute(0, 2, 1)
+        #print('v_sum.size() : ', v_sum.size()) # v_sum.size() :  torch.Size([2, 992, 256])
+        b, ch, hw = v_sum.size()
+        v_sum = v_sum.reshape((b, ch, 16, 16))
+        #print('v_sum.size() : ', v_sum.size())  # v_sum.size() :  torch.Size([2, 992, 16, 16])
+        return v_sum
+
+class Combined_Model(nn.Module):
+    """Discriminator network with PatchGAN.
+    W = (W - F + 2P) /S + 1"""
+
+    def __init__(self, in_channels=3, spec_norm=False, LR=0.2):
+        super(Combined_Model, self).__init__()
+
+    def forward(self, x):
+        return x
--- testing.py
+++ testing.py
@@ -4,9 +4,10 @@ import numpy as np
 from utils import get_outline_image
 from utils import elastic_transform
 from PIL import Image
-
+from model import Encoder
+from model import SCFT_Moudle
 import cv2
-
+import torch.nn as nn
 vgg_activation = dict()
 
 def get_activation(name):
@@ -16,6 +17,7 @@ def get_activation(name):
     return hook
 
 if __name__ == '__main__':
+    pass
     """
     model = vgg19()
     model.features[3].register_forward_hook(get_activation(str('relu_3')))
@@ -33,8 +35,8 @@ if __name__ == '__main__':
 
     #result = get_outline_image(r"C:\Users\woodc\Desktop\deep_paint_split\train\1020_color.png").convert('RGB')
     #result = np.array(result)
-
-    result = Image.open(r"C:\Users\woodc\Desktop\deep_paint_split\train\1038_color.png").convert("RGB")
+    #result = Image.open(r"C:\Users\woodc\Desktop\deep_paint_split\train\1038_color.png").convert("RGB")
+    """
     result = np.asarray(result)
     elastic_result = elastic_transform(result, 5000, 8, random_state=None)
     cv2.namedWindow('elastic', 0)
@@ -44,14 +46,29 @@ if __name__ == '__main__':
     elastic_result.save('./elastic_result.jpg')
     result = np.array(result)
     uniform_result = np.random.uniform(-50, 50, np.shape(result))
-    #print(uniform_result)
-    #print(np.shape(result))
-    #print(np.shape(uniform_result))
-
+    print(uniform_result)
+    print(np.shape(result))
+    print(np.shape(uniform_result))
     sum = result + uniform_result
     sum = sum.astype('uint8')
     noised = Image.fromarray(sum)
     noised.save('./noised_result.jpg')
-    #uniform = (-50. - 50.) * torch.randn(10)
-    #uniform = torch.FloatTensor(10).uniform_(-50, 50)
-    #print(uniform)
+
+    uniform = (-50. - 50.) * torch.randn(10)
+    uniform = torch.FloatTensor(10).uniform_(-50, 50)
+    print(uniform)
+    """
+
+    model_r = Encoder()
+    model_s = Encoder(in_channels=1)
+    tensor1 = torch.randn((2,3,256,256))
+    tensor2 = torch.randn((2,1,256,256))
+    v_r, _ = model_r(tensor1)
+    v_s, _ = model_s(tensor2)
+
+    scft_module = SCFT_Moudle()
+    scft_module(v_s, v_r)
+
+
+
+
